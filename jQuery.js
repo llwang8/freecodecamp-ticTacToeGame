06e75f2ddead45a48d713b4e8d9f0f6747f26994
llwang8 <wangxbl56@gmail.com>
2016-07-24 22:40:07 -0400

    add js code to check if there is a winner

--- jQuery.js
+++ jQuery.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     playerSymbol = prompt("X XOR O?");
     computerSymbol = (playerSymbol === "X") ? "O" : "X";
 
-
     if (computerTurn) {
         computerMove();
     }else {
@@ -27,33 +26,48 @@ $(document).ready(function(){
     });
     //computer move
     function computerMove(){
-
+        if ()
     }
 
     //check if there is a win
+    function hint(msg){
+
+    }
+
     function checkWin(){
         if (player.length < 3 && computer.length < 3){
             return;
         }else if (player.length === 3) {
-            if (allMatch(player)){
-                displayWin(player);
-            }
+            allMatch(player));
         }else {
-            if (allMatch(computer)){
-                displayWin(computer);
-            }
+            allMatch(computer);
         }
     }
 
     function allMatch(arr){
+        if (arr[0].charAt(0) === arr[1].charAt(0) && arr[0].charAt(0) === arr[2].charAt(0)){
+            displayWin()    // row match
+        }
+        if (arr[0].charAt(1) === arr[1].charAt(1) && arr[0].charAt(1) === arr[2].charAt(1)){
+            displayWin() // col match
+        }
+        if (arr.indexOf("22") >= 0){
+            if (arr.indexOf('13') >= 0  && arr.indexOf('31') >= 0){
+                displayWin(); // diagnol match
+            }else if (arr.indexOf('11') && arr.indexOf('33') >= 0) {
+               displayWin(); // diagnol match
+            }
+
+        }
 
     }
 
-    function displayWin(arr){
+    function displayWin(){
         //draw animated line
 
         //reset grid, check who moves first
 
+        computerTurn != computerTurn;
     }
 
 
